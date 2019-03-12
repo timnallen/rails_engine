1fc880f2e89eeb935891235fdbeb153b012fd099
Tim Allen <tnelsallen3@gmail.com>
2019-03-11 18:53:42 -0600

    Builds tests for query params and id and name are passing

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+describe "Merchants API" do
+  it "sends a list of merchants" do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+    merchants = JSON.parse(response.body)
+    expect(merchants.count).to eq(3)
+  end
+
+  it "can get one merchant by its id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["id"]).to eq(id)
+  end
+
+  it "can get one merchant by searching its id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/find?id=#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["id"]).to eq(id)
+  end
+
+  it "can get one merchant by searching its name" do
+    name = create(:merchant).name
+
+    get "/api/v1/merchants/find?name=#{name}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["name"]).to eq(name)
+  end
+
+  xit "can get one merchant by searching its created_at date" do
+    created_at = create(:merchant).created_at
+    binding.pry
+
+    get "/api/v1/merchants/find?created_at=#{created_at}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["created_at"]).to eq(created_at)
+  end
+
+  xit "can get one merchant by searching its updated_at date" do
+    updated_at = create(:merchant).updated_at
+    binding.pry
+
+    get "/api/v1/merchants/find?updated_at=#{updated_at}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["updated_at"]).to eq(updated_at)
+  end
+end
