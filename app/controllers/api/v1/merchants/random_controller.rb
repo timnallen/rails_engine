43327e64ab11b39c7b616cd3e58a2e038cbb8576
Tim Allen <tnelsallen3@gmail.com>
2019-03-12 13:28:49 -0600

    Adds limit to AR call for random merchant

--- app/controllers/api/v1/merchants/random_controller.rb
+++ app/controllers/api/v1/merchants/random_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Merchants::RandomController < ApplicationController
   def show
-    render json: Merchant.order(Arel.sql("RANDOM()")).first
+    render json: Merchant.order(Arel.sql("RANDOM()")).limit(1).first
   end
 end
