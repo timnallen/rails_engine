a06b2d64c93d03486e48fa0ed8712a790d39b077
Tim Allen <tnelsallen3@gmail.com>
2019-03-15 10:43:00 -0600

    Adds invoice to invoice item assoc spec

---
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'Invoice Items API' do
+  describe 'relationship endpoints' do
+    before :each do
+      customer = create(:customer)
+      merchant = create(:merchant)
+      @invoice = create(:invoice, merchant: merchant, customer: customer)
+      @item = create(:item, merchant: merchant)
+      @invoice_item = create(:invoice_item, item: @item, invoice: @invoice)
+    end
+
+    it 'can get the associated invoice for an invoice_item' do
+      get "/api/v1/invoice_items/#{@invoice_item.id}/invoice"
+
+      invoice = JSON.parse(response.body)['data']
+
+      expect(response).to be_successful
+      expect(invoice['id']).to eq(@invoice.id.to_s)
+    end
+  end
+end
