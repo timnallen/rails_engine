14e0c724677f62869bf0490d73f49b1233e9940a
Tim Allen <tnelsallen3@gmail.com>
2019-03-15 10:12:02 -0600

    Adds invoice items for an invoice spec

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -5,23 +5,38 @@ describe "Invoices API" do
   end
 
   describe 'relationship endpoints' do
-    it 'can get all the transactions for an invoice' do
-      customer = create(:customer)
-      merchant = create(:merchant)
-      item = create(:item, merchant: merchant)
-      invoice = create(:invoice, customer: customer, merchant: merchant)
-      create(:invoice_item, item: item, invoice: invoice)
-      transaction_1 = create(:transaction, invoice: invoice, result: 'failed')
-      transaction_2 = create(:transaction, invoice: invoice, result: 'success')
+    before :each do
+      @customer = create(:customer)
+      @merchant = create(:merchant)
+      @item_1 = create(:item, merchant: @merchant)
+      @item_2 = create(:item, merchant: @merchant)
+      @invoice = create(:invoice, customer: @customer, merchant: @merchant)
+      @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice)
+      @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice)
+      @transaction_1 = create(:transaction, invoice: @invoice, result: 'failed')
+      @transaction_2 = create(:transaction, invoice: @invoice, result: 'success')
+    end
 
-      get "/api/v1/invoices/#{invoice.id}/transactions"
+    it 'can get all the transactions for an invoice' do
+      get "/api/v1/invoices/#{@invoice.id}/transactions"
 
       transactions = JSON.parse(response.body)['data']
 
       expect(response).to be_successful
       expect(transactions.count).to eq(2)
-      expect(transactions[0]['id']).to eq(transaction_1.id.to_s)
-      expect(transactions[1]['id']).to eq(transaction_2.id.to_s)
+      expect(transactions[0]['id']).to eq(@transaction_1.id.to_s)
+      expect(transactions[1]['id']).to eq(@transaction_2.id.to_s)
+    end
+
+    it 'can get all the invoice_items for an invoice' do
+      get "/api/v1/invoices/#{@invoice.id}/invoice_items"
+
+      invoice_items = JSON.parse(response.body)['data']
+
+      expect(response).to be_successful
+      expect(invoice_items.count).to eq(2)
+      expect(invoice_items[0]['id']).to eq(@invoice_item_1.id.to_s)
+      expect(invoice_items[1]['id']).to eq(@invoice_item_2.id.to_s)
     end
   end
 end
