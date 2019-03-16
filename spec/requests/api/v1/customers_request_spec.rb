914dca23a8dda2957715e2c9ce6cece64a8dcfe3
Tim Allen <tnelsallen3@gmail.com>
2019-03-16 09:09:12 -0600

    Adds Spec and sets up customers index

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 describe "Customers API" do
   describe 'record endpoints' do
+    it "can get a list of customers" do
+      create_list(:customer, 3)
+
+      get '/api/v1/customers'
+
+      expect(response).to be_successful
+      customers = JSON.parse(response.body)
+      expect(customers['data'].count).to eq(3)
+    end
   end
 
   describe 'relationship endpoints' do
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "Merchants API" do
   describe 'record endpoints' do
-    it "sends a list of merchants" do
+    it "can get a list of merchants" do
       create_list(:merchant, 3)
 
       get '/api/v1/merchants'
