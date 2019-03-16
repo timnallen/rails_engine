55f811e66eb46d3f6f3478f6655e86a491c96fb7
Tim Allen <tnelsallen3@gmail.com>
2019-03-16 09:47:48 -0600

    Adds customer random feature

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -79,13 +79,76 @@ describe "Customers API" do
       expect(customer_json['data']['attributes']['first_name']).to eq(customer.first_name)
       expect(customer_json['data']['id']).to eq(customer.id.to_s)
     end
+
+    it 'can get all customers by searching by id or name' do
+      id = create(:customer).id.to_s
+
+      get "/api/v1/customers/find_all?id=#{id}"
+
+      customers = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(customers['data'][0]["id"]).to eq(id)
+
+      first_name = create(:customer).first_name
+
+      get "/api/v1/customers/find_all?first_name=#{first_name}"
+
+      customers = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(customers['data'][0]['attributes']["first_name"]).to eq(first_name)
+      expect(customers['data'][1]['attributes']["first_name"]).to eq(first_name)
+      expect(customers['data'].count).to eq(2)
+    end
+
+    it 'can get all customers by searching by created_at or updated_at' do
+      customer = create(:customer, created_at: "2012-03-27 14:54:05 UTC", updated_at: "2012-03-27 14:54:05 UTC".to_datetime)
+
+      get "/api/v1/customers/find_all?created_at=#{customer.created_at}"
+
+      customers = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(customers['data'][0]['attributes']["first_name"]).to eq(customer.first_name)
+
+      customer_2 = create(:customer, updated_at: "2012-03-27 14:54:05 UTC")
+
+      get "/api/v1/customers/find_all?updated_at=#{customer_2.updated_at}"
+
+      customers = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(customers['data'][0]['attributes']["first_name"]).to eq(customer.first_name)
+      expect(customers['data'][1]['attributes']["first_name"]).to eq(customer_2.first_name)
+      expect(customers['data'].count).to eq(2)
+    end
+
+    it 'can get a random customer' do
+      id = create(:customer).id.to_s
+
+      get "/api/v1/customers/random"
+
+      customer = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(customer['data']["id"]).to eq(id)
+
+      id_2 = create(:customer).id.to_s
+
+      get "/api/v1/customers/random"
+
+      customer_2 = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(customer_2['data']["id"]).to be_in([id, id_2])
+    end
   end
 
   describe 'relationship endpoints' do
     before :each do
       @customer = create(:customer)
       @customer_2 = create(:customer)
-      @merchant = create(:merchant)
       @merchant = create(:merchant)
       @invoice_1 = create(:invoice, customer: @customer, merchant: @merchant)
       @invoice_2 = create(:invoice, customer: @customer, merchant: @merchant)
